16616f66d8c2bea537e73f25b7dab68d8efe355d
Vitor Freitas <vitorfs@gmail.com>
2017-09-13 23:25:04 +0300

    Add base template, change logo font, add url tests

--- boards/tests.py
+++ boards/tests.py
@@ -7,14 +7,21 @@ from .models import Board
 
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
@@ -34,3 +41,9 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
