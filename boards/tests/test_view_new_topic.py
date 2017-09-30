8023a88e8edd8445269f7f3f62b97e5790739dc1
Vitor Freitas <vitorfs@gmail.com>
2017-09-30 19:34:56 +0300

    Add login required test new_topic view

--- boards/tests/test_view_new_topic.py
+++ boards/tests/test_view_new_topic.py
@@ -79,3 +79,14 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+
+class LoginRequiredNewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+        self.url = reverse('new_topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
