dc7aa18bdf00450d20caed30ea58420452aaad93
Vitor Freitas <vitorfs@gmail.com>
2017-10-01 20:35:19 +0300

    Migrate topic model

--- boards/models.py
+++ boards/models.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import Truncator
 
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    slug = models.SlugField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 
 class Topic(models.Model):
@@ -16,6 +22,10 @@ class Topic(models.Model):
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.subject
 
 
 class Post(models.Model):
@@ -25,3 +35,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -12,7 +13,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
 @login_required
@@ -38,6 +40,8 @@ def new_topic(request, pk):
 
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
 
