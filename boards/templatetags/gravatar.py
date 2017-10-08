02ca589db81bd1813b55d90d7a6c9eb9bb3df8d1
Vitor Freitas <vitorfs@gmail.com>
2017-10-08 21:47:41 +0300

    Add gravatar

---
+++ boards/templatetags/gravatar.py
@@ -0,0 +1,19 @@
+import hashlib
+from urllib.parse import urlencode
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def gravatar(user):
+    email = user.email.lower().encode('utf-8')
+    default = 'mm'
+    size = 256
+    url = 'https://www.gravatar.com/avatar/{md5}?{params}'.format(
+        md5=hashlib.md5(email).hexdigest(),
+        params=urlencode({'d': default, 's': str(size)})
+    )
+    return url
