5c2a6b4138de0a4fa8211e7c6015aa556b0bff24
Vitor Freitas <vitorfs@gmail.com>
2017-10-16 00:55:40 +0300

    Add default from email

--- myproject/settings.py
+++ myproject/settings.py
@@ -111,3 +111,6 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'Django Boards <noreply@djangoboards.com>'
+EMAIL_SUBJECT_PREFIX = '[Django Boards] '
