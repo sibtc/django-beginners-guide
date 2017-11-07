16aa1d5e44b7caf611f9f9d1afcd28c9cc0d2f6f
Mohit <mohitsolanki619@gmail.com>
2017-11-08 00:27:31 +0530

    Update Views to use LoginRequiredMixin

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import SignUpForm, UserInformationUpdateForm
 
@@ -21,8 +19,7 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 
-@method_decorator(login_required, name='dispatch')
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, UpdateView):
     form_class = UserInformationUpdateForm
     template_name = 'my_account.html'
     success_url = reverse_lazy('my_account')
